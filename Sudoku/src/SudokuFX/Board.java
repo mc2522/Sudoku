3224ca5d0783e82fd501d6cb81fd39723d48e519
Mike Cao <mc2522@rit.edu>
2019-12-27 15:39:39 -0500

    Board class created. Need to work on random generation.

--- Sudoku/src/SudokuFX/Board.java
+++ Sudoku/src/SudokuFX/Board.java
@@ -1,5 +1,15 @@
 package SudokuFX;
 
 public class Board {
-    
+    // 2D array to represent sudoku board
+    private int board[][];
+
+    /**
+     * Constructor for Board
+     * Initialize board with a 2D array of 9x9 size
+     */
+    public Board() {
+        board = new int[9][9];
+    }
+
 }
--- Sudoku/src/SudokuFX/SudokuPTUI.java
+++ Sudoku/src/SudokuFX/SudokuPTUI.java
@@ -3,7 +3,8 @@ package SudokuFX;
 public class SudokuPTUI {
 
     public static void main(String [] args) {
-
+        // create a new board
+        Board board = new Board();
     }
 
 }
