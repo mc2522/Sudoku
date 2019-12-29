322ceb7acc3858a4b845c11da9a2f7b705d89c2d
Mike Cao <mc2522@rit.edu>
2019-12-29 14:15:50 -0500

    Removed most of unnecessary functions.

--- Sudoku/src/SudokuFX/Checker.java
+++ Sudoku/src/SudokuFX/Checker.java
@@ -1,8 +1,6 @@
 package SudokuFX;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Random;
 
 public class Checker {
     private final int DIM = 9;
@@ -62,90 +60,6 @@ public class Checker {
             }
         }
         return nonet;
-    }
-
-    /**
-     * Get an ArrayList of integers that can be used.
-     * @param numbers - row/column to check
-     * @return availableNumbers - ArrayList of available numbers
-     */
-    public ArrayList<Integer> getAvailableNumbers(int [] numbers) {
-        ArrayList<Integer> availableNumbers = new ArrayList<>();
-        // Add all numbers 1-9 into an ArrayList
-        for (int number = 1; number < 10; number++)
-            availableNumbers.add(number);
-        // Remove all numbers that appeared from the ArrayList
-        for (int number : numbers) {
-            if (number != 0)
-                availableNumbers.remove((Integer)number);
-        }
-        return availableNumbers;
-    }
-
-    /**
-     * Get an ArrayList of integers that can be used.
-     * @param nonet - nonet to check
-     * @return availableNumbers - ArrayList of available numbers
-     */
-    public ArrayList<Integer> getAvailableNumbers(ArrayList<Integer> nonet) {
-        ArrayList<Integer> availableNumbers = new ArrayList<>();
-        // Add all numbers 1-9 into an ArrayList
-        for (int number = 1; number < 10; number++)
-            availableNumbers.add(number);
-        // Remove all numbers that appeared from the ArrayList
-        for (int number : nonet) {
-            if (number != 0)
-                availableNumbers.remove((Integer)number);
-        }
-        return availableNumbers;
-    }
-
-    /**
-     * generates a new integer that is unused in the row/column and nonet
-     * @param row - row or column of numbers
-     * @param nonet - nonet of numbers
-     * @return unused integer in row/column and nonet
-     */
-    public int generateAvailableNumber(int [] row, int [] column, ArrayList<Integer> nonet) {
-        ArrayList<Integer> rowAvailableNumbers = getAvailableNumbers(row);
-        //System.out.println("row an: " + rowAvailableNumbers);
-        ArrayList<Integer> nonetAvailableNumbers = getAvailableNumbers(nonet);
-        //System.out.println("nonet an: " + nonetAvailableNumbers);
-        ArrayList<Integer> columnAvailableNumbers = getAvailableNumbers(column);
-        //System.out.println("column an: " + columnAvailableNumbers);
-        ArrayList<Integer> commonNumbers = new ArrayList<>();
-        for (int number : rowAvailableNumbers) {
-            if (nonetAvailableNumbers.contains(number) && columnAvailableNumbers.contains(number))
-                commonNumbers.add(number);
-        }
-        //System.out.println(commonNumbers);
-        Random random = new Random();
-        int size = commonNumbers.size();
-        return commonNumbers.get(Math.abs(random.nextInt(size)));
-    }
-
-    /**
-     * generates a new integer that is unused in the row/column and nonet
-     * @param row - row or column of numbers
-     * @param nonet - nonet of numbers
-     * @return unused integer in row/column and nonet
-     */
-    public ArrayList<Integer> AvailableNumber(int [] row, int [] column, ArrayList<Integer> nonet) {
-        ArrayList<Integer> rowAvailableNumbers = getAvailableNumbers(row);
-        //System.out.println("row an: " + rowAvailableNumbers);
-        ArrayList<Integer> nonetAvailableNumbers = getAvailableNumbers(nonet);
-        //System.out.println("nonet an: " + nonetAvailableNumbers);
-        ArrayList<Integer> columnAvailableNumbers = getAvailableNumbers(column);
-        //System.out.println("column an: " + columnAvailableNumbers);
-        ArrayList<Integer> commonNumbers = new ArrayList<>();
-        for (int number : rowAvailableNumbers) {
-            if (nonetAvailableNumbers.contains(number) && columnAvailableNumbers.contains(number))
-                commonNumbers.add(number);
-        }
-        //System.out.println(commonNumbers);
-        Random random = new Random();
-        int size = commonNumbers.size();
-        return commonNumbers;
     }
 
     /**
